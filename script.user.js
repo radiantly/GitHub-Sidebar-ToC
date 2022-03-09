64538cb5aedffac64a0d6bfd712c58d6289fe90e
radiantly <buildingsomethingfun@gmail.com>
2022-03-09 22:56:44 +0530

    Hide ToC when no items exist

--- script.user.js
+++ script.user.js
@@ -4,7 +4,7 @@
 // @match       https://github.com/*/*
 // @match       https://github.com/*/*/tree/*
 // @grant       GM_addStyle
-// @version     1.1
+// @version     1.2
 // @author      radiantly
 // @description Puts an interactive Table of Contents on the side of a GitHub Readme!
 // ==/UserScript==
@@ -46,31 +46,29 @@ GM_addStyle(`
 `);
 
 // Add the ToC section to the sidebar!
-const sidebarGrid = document.querySelector(".Layout-sidebar .BorderGrid");
-sidebarGrid.style.height = "100%";
-sidebarGrid.insertAdjacentHTML(
-  "beforeend",
-  `
-<div class="lb-toc-bgrow BorderGrid-row js-position-sticky">
-  <div class="BorderGrid-cell">
-    <h2 class="h4">Table of Contents</h2>
-    <div class="lb-toc-wrap pt-3 pb-3">
-      <div class="lb-toc-items"></div>
+const addToCSection = () => {
+  const sidebarGrid = document.querySelector(".Layout-sidebar .BorderGrid");
+  sidebarGrid.style.height = "100%";
+  sidebarGrid.insertAdjacentHTML(
+    "beforeend",
+    `
+  <div class="lb-toc-bgrow BorderGrid-row js-position-sticky">
+    <div class="BorderGrid-cell">
+      <h2 class="h4">Table of Contents</h2>
+      <div class="lb-toc-wrap pt-3 pb-3">
+        <div class="lb-toc-items"></div>
+      </div>
     </div>
   </div>
-</div>
-`
-);
+  `
+  );
+};
 
 const anchorElems = [];
 
-const tocWrap = document.querySelector(".lb-toc-items");
-const tocItems = document.querySelectorAll(
-  ".SelectMenu-list > a.SelectMenu-item"
-);
-
 let currentActive = null;
 let justClicked = false;
+
 const setActive = (idx) => {
   if (currentActive === idx) return;
   if (currentActive !== null)
@@ -79,50 +77,60 @@ const setActive = (idx) => {
   currentActive = idx;
 };
 
-tocItems.forEach((elem, index) => {
-  tocWrap.insertAdjacentHTML(
-    "beforeend",
-    `<a href="${elem.href}" style="padding-left: ${elem.style.paddingLeft}">${elem.innerText}</a>`
-  );
-  const tocItem = document.querySelector(
-    `.lb-toc-items a[href="${elem.href}"]`
-  );
-  tocItem.addEventListener("click", (e) => {
-    setActive(index);
-    justClicked = true;
-  });
-  anchorElems.push({
-    headingElem: document.querySelector(
-      `.anchor[href="${elem.href.replace(/^[^#]*/, "")}"]`
-    ),
-    listElem: tocItem,
-  });
-});
-
-setActive(0);
-
-document.addEventListener(
-  "scroll",
-  (e) => {
-    // Don't change the active toc item if the user just clicked it
-    if (justClicked) return (justClicked = false);
-
-    const offsetTop = 55; // offset top due to sticky header
-
-    // Highlight the correct active toc item on scroll
-    // Binary Search FTW!
-    let low = 0;
-    let high = anchorElems.length - 1;
-    while (low <= high) {
-      const mid = Math.floor((low + high) / 2);
-      if (anchorElems[mid].headingElem.getBoundingClientRect().top > offsetTop)
-        high = mid - 1;
-      else low = mid + 1;
-    }
-
-    return setActive(high + 1);
-  },
-  {
-    passive: true,
-  }
+const tocItems = document.querySelectorAll(
+  ".SelectMenu-list > a.SelectMenu-item"
 );
+
+const populateToC = () => {
+  const tocWrap = document.querySelector(".lb-toc-items");
+  tocItems.forEach((elem, index) => {
+    tocWrap.insertAdjacentHTML(
+      "beforeend",
+      `<a href="${elem.href}" style="padding-left: ${elem.style.paddingLeft}">${elem.innerText}</a>`
+    );
+    const tocItem = document.querySelector(
+      `.lb-toc-items a[href="${elem.href}"]`
+    );
+    const headingElem = document.querySelector(
+      `.anchor[href="${elem.href.replace(/^[^#]*/, "")}"]`
+    );
+    tocItem.addEventListener("click", (e) => {
+      setActive(index);
+      justClicked = true;
+    });
+    anchorElems.push({
+      headingElem,
+      listElem: tocItem,
+    });
+  });
+};
+
+const handleScroll = () => {
+  // Don't change the active toc item if the user just clicked it
+  if (justClicked) return (justClicked = false);
+
+  const offsetTop = 55; // offset top due to sticky header
+
+  // Highlight the correct active toc item on scroll
+  // Binary Search FTW!
+  let low = 0;
+  let high = anchorElems.length - 1;
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const curTop = anchorElems[mid].headingElem.getBoundingClientRect().top;
+    if (curTop > offsetTop) high = mid - 1;
+    else low = mid + 1;
+  }
+
+  return setActive(Math.max(0, high));
+};
+
+(() => {
+  if (tocItems.length === 0) return;
+  addToCSection();
+  populateToC();
+  setActive(0);
+  document.addEventListener("scroll", handleScroll, {
+    passive: true,
+  });
+})();
